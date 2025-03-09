eaf44d862cb60318b60a39e78784a7d7e03f813a
samarjeetpatankar <samarjeetpatankar@gmail.com>
2025-03-09 08:53:29 +0530

    commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  userCredits,
+  paymentRazorPay,
+} from "../controllers/userController.js";
+import userAuth from "../middlewares/auth.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/credits", userAuth, userCredits);
+userRouter.post("/pay-razor", userAuth, paymentRazorPay);
+
+export default userRouter;
