0dc7a1235c3f1f1c185d6d6d5219cc6d03f2362a
samarjeetpatankar <samarjeetpatankar@gmail.com>
2025-03-11 20:16:44 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
+//import connectDB from './config/mongodb.js'
 // import userRouter from './routes/userRoutes.js'
 // import imageRouter from './routes/imageRoutes.js'
 
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-await connectDB()
+//await connectDB()
 // app.use('/api/user', userRouter)
 // app.use('/api/image', imageRouter)
 app.get('/', (req, res)=>{
