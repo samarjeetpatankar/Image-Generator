b0abe021fcbb68abdd5b41c9e2d406f2f3f6dc00
samarjeetpatankar <samarjeetpatankar@gmail.com>
2025-03-11 19:38:24 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
-import userRouter from './routes/userRoutes.js'
-import imageRouter from './routes/imageRoutes.js'
+// import userRouter from './routes/userRoutes.js'
+// import imageRouter from './routes/imageRoutes.js'
 
 const PORT = process.env.PORT
 const app = express()
@@ -12,8 +12,8 @@ app.use(express.json())
 app.use(cors())
 
 await connectDB()
-app.use('/api/user', userRouter)
-app.use('/api/image', imageRouter)
+// app.use('/api/user', userRouter)
+// app.use('/api/image', imageRouter)
 app.get('/', (req, res)=>{
     return res.send("API WORKING")
 })
